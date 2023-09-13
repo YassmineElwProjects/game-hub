a3d517d0383332084790ca25e70b71f64b66c2e7
YassmineelwProjects <yassmelw@gmail.com>
2023-09-13 17:43:52 -0400

    Build a responsive Layout

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
