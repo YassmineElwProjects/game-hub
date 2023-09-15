5c74268f9b559caa96b74b0436646aaa9e378d1b
YassmineelwProjects <yassmelw@gmail.com>
2023-09-15 19:51:45 -0400

    Adding pictures to the genres

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,27 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import { Navbar } from "./components/Navbar";
 import { GameGrid } from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import GenreListItems from "./components/GenreList";
 
 function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+
+      templateColumns={ {
+        base: '1fr',
+        lg: '200px',
+      } }
+
     >
       <GridItem area="nav">
         <Navbar />
       </GridItem>
       <Show above="lg">
+        <GridItem area={"aside"} paddingX={5}>
         <GenreList />
+        </GridItem>
+        
       </Show>
       <GridItem area="main">
         <GameGrid />
