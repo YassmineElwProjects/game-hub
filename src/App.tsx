4b8fc656fbfd59d79b8a8fe4c66b0b6059d7e15b
YassmineelwProjects <yassmelw@gmail.com>
2023-09-16 01:12:22 -0400

    Fix error with import HStack

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Show } from "@chakra-ui/react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Navbar } from "./components/Navbar";
 import { GameGrid } from "./components/GameGrid";
@@ -38,19 +38,23 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-
-          <PlatformSelector
-            selectedPlatform={gameQuery.platform}
-            onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })}
+        <Flex paddingLeft={2} marginBottom={5}>
+          <Box marginRight={5}>
+            <PlatformSelector
+              selectedPlatform={gameQuery.platform}
+              onSelectPlatform={(platform) =>
+                setGameQuery({ ...gameQuery, platform })
+              }
+            />
+          </Box>
+          <SortSelector
+            sortOrder={gameQuery.sortOrder}
+            onSelectSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
           />
-
-          <SortSelector sortOrder={gameQuery.sortOrder}  onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
-        </HStack>
-        <GameGrid
-          gameQuery={gameQuery}
-        />
-
+        </Flex>
+        <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
   );
